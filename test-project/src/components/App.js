b32dfb4918d8350381e9f50c07dd37477288cb3c
shillyshaly <litoxl@hotmail.com>
2022-09-21 19:13:25 -0700

    first commit

---
+++ test-project/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>oh fuck off!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
